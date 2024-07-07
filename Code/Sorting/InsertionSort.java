fbd723660450ee7865d116125190efcf4f04c203
hsharma-25 <143306560+hsharma-25@users.noreply.github.com>
2024-07-07 17:49:44 +0530

    Add files via upload

---
+++ Code/Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args){
+
+        int[] arr= {7,3,8,1,2};
+
+        System.out.println("Sorted array: ");
+
+        //Insertion Sort
+        for(int i=1; i<arr.length; i++){
+            int current=arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>current){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=current;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ Code/Sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class SelectionSort {
+    public static void main(String[] args){
+
+        int[] arr= {7,3,8,1,2};
+
+        System.out.println("Sorted array: ");
+
+        //Selection Sort
+        for(int i=0; i<arr.length-1; i++){
+            int smallest=i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            }
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
